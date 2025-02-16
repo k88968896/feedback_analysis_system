52f9ea4980dfe23f032642b854e40c78ad2d61de
Jerry <s5237981123@gmail.com>
2025-02-16 19:02:46 +0800

    v2

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,7 +3,7 @@ import Layout from "../components/Layout";
 const Home = () => {
     return (
         <Layout>
-            <h2>歡迎使用管理系統</h2>
+            <h2>歡迎使用回饋分析系統</h2>
             <p>請選擇左側選單進行操作。</p>
         </Layout>
     );
